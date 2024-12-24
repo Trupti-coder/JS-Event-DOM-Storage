e6a5dac2e679710d362f92814a943e6f82c5eeb8
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-24 09:03:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 var btn=document.getElementById("btnAdd");
+btn.addEventListener("click",funAdd,false);
+
+function funAdd(){
+    var txtName=document.getElementById("txtId").value;
+    var txtPrice=document.getElementById("txtPrice").value;
+    console.log(txtName,txtPrice);
+
+}
