40fc9d0b63c87d71ee618c5f9908640062491027
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-24 22:50:52 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var btn=document.getElementById("btnAdd");
-btn.addEventListener("click",funAdd,false);
-
-function funAdd(){
-    var txtName=document.getElementById("txtId").value;
-    var txtPrice=document.getElementById("txtPrice").value;
-    console.log(txtName,txtPrice);
-
-}
