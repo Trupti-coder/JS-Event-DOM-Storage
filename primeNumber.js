db0a14a5933364a41476e2814ea152fcb2736eaa
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-25 01:20:47 -0500

    Create primeNumber.js

---
+++ primeNumber.js
@@ -0,0 +1,8 @@
+
+//A number that is fully divisible only by 1 and itself is called a prime number.
+//  It cannot be divided evenly by any other number, including 2, unless it is 2 itself.
+
+function primeNumber(num){
+   
+    
+}
