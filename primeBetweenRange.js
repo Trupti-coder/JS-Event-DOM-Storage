d806829b0f44b7f5928f086fd661c3bade69c6f1
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-25 01:44:56 -0500

    Update primeBetweenRange.js

--- primeBetweenRange.js
+++ primeBetweenRange.js
@@ -1,8 +1,33 @@
-function primeNumber(start,end){
 
-    for(let i=start;i<=end;i++){
-        
+function primeNumber(num){
+   
+    if(num<1){
+        return false;
     }
 
+    for(let i=2;i<num;i++){
+        if(num%i===0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function primeNumberRange(start,end){
+let primes=[];
+    for(let num=start;num<=end;num++){
+
+        if(primeNumber(num)){
+            primes[primes.length]=num;
+        }
+        
+
+
+    }
+return primes;
 
 }
+
+console.log(primeNumberRange(10,50));
+//Prime numbers between 10 and 50: [ 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]
